c87677f32fe26e06dcbfbf0b0118f77bd31eb2ae
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-05-03 17:21:27 +0530

    Refactor page layout and styles, update global styles, and add theme context

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// import type { Config } from "tailwindcss";
+
+const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
-    container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: {
@@ -38,7 +45,7 @@ module.exports = {
       },
       boxShadow: {
         "light-100":
-          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px",
+          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)",
         "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
         "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
         "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
@@ -53,11 +60,13 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
+          // @ts-ignore
           from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
+          // @ts-ignore
           to: { height: 0 },
         },
       },
@@ -67,4 +76,6 @@ module.exports = {
       },
     },
   },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
+export default config;
