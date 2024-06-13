dc991737d2727d94704fe74ec0ead89d19500cd7
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-06-13 17:32:51 +0530

    refactor: Update webhook route for Clerk to use NEXT_CLERK_WEBHOOK_SECRET

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/webhook/clerk",
+    "/api/rapidapi",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook/clerk", "/api/openai", "/api/rapidapi"],
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
